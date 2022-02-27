9f2c277c10a4f4dafdf765320d56823f4a0afbad
wlillisdev <williamlillis100@gmail.com>
2022-02-27 10:59:10 +0000

    defined function for play hangman game function

--- run.py
+++ run.py
@@ -3,6 +3,8 @@
 import random
 # import words
 from words import words
+# import libary of ascii_letters
+import string
 
 
 def get_word():
@@ -12,15 +14,21 @@ def get_word():
     """
     # Select a randon word from the list
     word = random.choice(words)
-    # Return word in uppercase
-    return word.upper()
+    # Return word in uppercase 
+    return word.upper() 
     
 
     
 
-def play_hangman(word):
+def play_hangman():
     """
     Play a game of hangman.
     Displays  lives and layout for player to play game.
     """
+    word = get_word() # get random word from get_word
+    word_letters = set(word)  # letters that are in word
+    alphabet = set(string.ascii_uppercase) # list of character in alphabet uppercase
+    used_letters = set()  # letters that the user has guessed
+
+    lives = 7   # number of tries the user has before game over
     
